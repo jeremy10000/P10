5612073b7487c8bb36b9c2c64995d81e6db7d152
Jérémy <jeremyjulienvisio@outlook.fr>
2020-01-13 01:51:59 +0100

    Command update

--- product/management/commands/update-db.py
+++ product/management/commands/update-db.py
@@ -18,6 +18,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """ usage : ./manage.py update-db """
+
         if STDOUT:
             self.stdout.write(self.style.SUCCESS(
                 'Check for updates...')
@@ -27,9 +28,14 @@ class Command(BaseCommand):
 
         for p in Product.objects.all():
             try:
+                proxies = {'http': '', 'https': 'https://62.210.124.248:3128'}
+                headers = {'User-Agent': 'Chrome'}
                 r = requests.get(
                     "https://fr.openfoodfacts.org/api/v0/product/"
-                    "{}.json".format(p.code), timeout=5.0, verify=False).json()["product"]
+                    "{}.json".format(p.code),
+                    headers=headers,
+                    proxies=proxies,
+                    timeout=1).json()["product"]
 
                 self.check_update(r, p)
 
@@ -38,10 +44,11 @@ class Command(BaseCommand):
                     self.stdout.write(self.style.SUCCESS(
                         'Exception : "%s", %s' % (p.code, e))
                     )
+                pass
 
         if STDOUT:
             self.stdout.write(self.style.SUCCESS(
-                'Update(s) : %s.' % counter)
+                'Update(s) : %s.' % self.counter)
             )
 
     def check_update(self, r, p):
@@ -49,35 +56,34 @@ class Command(BaseCommand):
         Args:
             r : requests response
             p : product
-            counter : self.counter
         """
         if int(p.last_modified_t) < int(r.get("last_modified_t", 0)):
             try:
                 n = r.get("nutriments")
                 nl = r.get("nutrient_levels")
 
-                p.name = r.get("product_name_fr", "")
-                p.url = r.get("url", "")
-                p.nutriscore = str(r.get("nutrition_grade_fr", "")) \
+                p.name = r.get("product_name_fr")
+                p.url = r.get("url")
+                p.nutriscore = str(r.get("nutrition_grade_fr")) \
                  .replace("'", "") \
                  .replace("[", "") \
                  .replace("]", "")
-                p.photo = r.get("image_url", "")
-                p.last_modified_t = r.get("last_modified_t", "")
+                p.photo = r.get("image_url")
+                p.last_modified_t = r.get("last_modified_t")
 
-                p.salt_100g = n.get("salt_100g", "")
-                p.sugars_100g = n.get("sugars_100g", "")
-                p.fat_100g = n.get("fat_100g", "")
-                p.saturate_fat_100g = n.get("saturated-fat_100g", "")
+                p.salt_100g = n.get("salt_100g")
+                p.sugars_100g = n.get("sugars_100g")
+                p.fat_100g = n.get("fat_100g")
+                p.saturate_fat_100g = n.get("saturated-fat_100g")
 
-                p.level_salt = Level.objects.get(name=nl.get("salt", ""))
+                p.level_salt = Level.objects.get(name=nl.get("salt"))
 
                 p.level_sugars = Level \
-                 .objects.get(name=nl.get("sugars", ""))
+                 .objects.get(name=nl.get("sugars"))
 
                 p.level_saturate_fat = Level.objects.get(
-                    name=nl.get("saturated-fat", ""))
-                p.level_fat = Level.objects.get(name=nl.get("fat", ""))
+                    name=nl.get("saturated-fat"))
+                p.level_fat = Level.objects.get(name=nl.get("fat"))
                 p.save()
 
                 if STDOUT:
@@ -86,8 +92,16 @@ class Command(BaseCommand):
                     )
                 self.counter += 1
 
-            except KeyError:
+            except AttributeError as e:
+                if STDOUT:
+                    self.stdout.write(self.style.SUCCESS(
+                        'AttributeError : "%s", %s' % (p.code, e))
+                    )
                 pass
 
-            except ValidationError:
+            except ValidationError as e:
+                if STDOUT:
+                    self.stdout.write(self.style.SUCCESS(
+                        'ValidationError : "%s", %s' % (p.code, e))
+                    )
                 pass
